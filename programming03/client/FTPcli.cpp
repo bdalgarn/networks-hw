8b6562bf0b8f1cac7f89b09e8226d6ba7a36b200
adifalco44 <adifalco@nd.edu>
2017-10-10 17:11:26 -0400

    Put client  dir funcs into main

--- programming03/client/FTPcli.cpp
+++ programming03/client/FTPcli.cpp
@@ -87,4 +87,110 @@ int main(int argc, char *argv[]){
     return 0;
 }
 
+void mdir(char *name, int32_t size){
+//    char buf[MAXSIZE]; 
+//    if ((size!=recv(new_s,buf,sizeof(buf),0))<0){
+//	fprintf(stderr,"[recv] : %s",strerror(errno));
+//	return -1;
+//    }
+//
+    char *size = atoi(size);
+   
+    char *init_msg = "";
+    sprintf(init_msg,"%s %s",size,name);
+
+    if (connect(sock, (struct sockaddr*)&server,sizeof(server)) == -1) {
+         perror("client: connect");
+         return EXIT_FAILURE;
+    }
+
+    bzero((char *)&buf, sizeof(buf));
+    strcpy(buf,init_msg,sizeof(init_msg));
+    if ((sendto(sock,buf,sizeof(buf),0,(struct sockaddr *)&server, sizeof(struct sockaddr))) < 0){
+            fprintf(stderr,"[Sendto] : %s",strerror(errno));
+            return EXIT_FAILURE;
+    }
+    if ((size=recv(sock,buf,sizeof(buf),0))<0){
+	fprintf(stderr,"[recv] : %s",strerror(errno));
+	return -1;
+    }
+    int num;
+    if (!strncmp(buf,"-2",2)){
+       fprintf(stdout,"The directory already exists on the server\n");
+       continue;
+    }else if (!strncmp(buf,"-1",2)){
+	fprintf(stdout,"Error making the directory\n");
+	continue;
+    }else{
+	fprintf(stdout,"The directory was successfully made\n");
+	continue;
+    }
+}
+
+void rdir(char *name, int32_t size){
+    char *size = atoi(size);
+   
+    char *init_msg = "";
+    sprintf(init_msg,"%s %s",size,name);
+
+    if (connect(sock, (struct sockaddr*)&server,sizeof(server)) == -1) {
+         perror("client: connect");
+         return EXIT_FAILURE;
+    }
+
+    bzero((char *)&buf, sizeof(buf));
+    strcpy(buf,init_msg,sizeof(init_msg));
+    if ((sendto(sock,buf,sizeof(buf),0,(struct sockaddr *)&server, sizeof(struct sockaddr))) < 0){
+            fprintf(stderr,"[Sendto] : %s",strerror(errno));
+            return EXIT_FAILURE;
+    }
+    if ((size=recv(sock,buf,sizeof(buf),0))<0){
+	fprintf(stderr,"[recv] : %s",strerror(errno));
+	return -1;
+    }
+    int num;
+    if (!strncmp(buf,"-2",2) || !strncmp(buf,"-1",2)){
+       fprintf(stdout,"Failed to delete directory\n");
+       continue;
+    }else{
+	fprintf(stdout,"The directory was successfully deleted\n");
+	continue;
+    }
+}
+
+void cdir(char *name, int32_t size){
+
+    char *size = atoi(size);
+   
+    char *init_msg = "";
+    sprintf(init_msg,"%s %s",size,name);
+
+    if (connect(sock, (struct sockaddr*)&server,sizeof(server)) == -1) {
+         perror("client: connect");
+         return EXIT_FAILURE;
+    }
+
+    bzero((char *)&buf, sizeof(buf));
+    strcpy(buf,init_msg,sizeof(init_msg));
+    if ((sendto(sock,buf,sizeof(buf),0,(struct sockaddr *)&server, sizeof(struct sockaddr))) < 0){
+            fprintf(stderr,"[Sendto] : %s",strerror(errno));
+            return EXIT_FAILURE;
+    }
+    if ((size!=recv(sock,buf,sizeof(buf),0))<0){
+	fprintf(stderr,"[recv] : %s",strerror(errno));
+	return -1;
+    }
+
+    if (!strncmp(buf,"-2",2)){
+       fprintf(stdout,"The directory does exist on the server\n");
+       continue;
+    }else if (!strncmp(buf,"-1",2)){
+	fprintf(stdout,"Error changing into the directory\n");
+	continue;
+    }else{
+	fprintf(stdout,"Successfully changed directory\n");
+	continue;
+    }
+
+}
 
--- programming03/server/myftpd.cpp
+++ programming03/server/myftpd.cpp
@@ -99,9 +99,99 @@ void dwld(char *buffer, int buf_len){}
 void upld(char *buffer, int buf_len){}
 void delf(char *buffer, int buf_len){}
 void list(){}
-void mdir(char *buffer, int buf_len){}
-void rdir(char *buffer, int buf_len){}
-void cdir(char *buffer, int buf_len){}
+void mdir(char *buffer, int buf_len){
+/*    char buf[MAXSIZE];
+    if ((size!=recv(new_s,buf,sizeof(buf),0))<0){
+	fprintf(stderr,"[recv] : %s",strerror(errno));
+	return -1;
+    }
+    char *root_path = "../root_dir"
+    char *full_path = "";
+    sprintf(full_path,"%s%s",root_path,name);
+    DIR *d = opendir(full_path);
+    if (d == NULL){ // Success
+	 char *command;
+	 char *temp_com = "mkdir ";
+         sprintf(command,"%s%s",temp_com,full_path);
+	 int rtr_val = system(command);
+	 if (rtr_val == 0){ // Successful Sys Call
+	    strcpy(buf,"1");	
+	 }else{             // Failed Sys Call
+	    strcpy(buf,"-1");
+	 }
+    }else{
+	strcpy(buf,"-2"); // Failure
+    } 
+*/    /* Sends Message */
+/*    if ((sendto(new_s,buf,sizeof(buf),0,(struct sockaddr *)&new_s,sizepf(struct sockaddr)))<0){
+	fprintf(stderr,"[MDIR sendto #1] : %s",strerror(errno));
+	continue;
+    }
+*/
+}
+void rdir(char *buffer, int buf_len){
+/*    char buf[MAXSIZE];
+    if ((size!=recv(new_s,buf,sizeof(buf),0))<0){
+	fprintf(stderr,"[recv] : %s",strerror(errno));
+	return -1;
+    }
+    char *root_path = "../root_dir"
+    char *full_path = "";
+    sprintf(full_path,"%s%s",root_path,name);
+    DIR *d = opendir(full_path);
+    if (d != NULL){ // Success
+	 char *command;
+	 char *temp_com = "rm ";
+         sprintf(command,"%s%s",temp_com,full_path);
+	 int rtr_val = system(command);
+	 if (rtr_val == 0){ // Successful Sys Call
+	    strcpy(buf,"1");	
+	 }else{             // Failed Sys Call
+	    strcpy(buf,"-1");
+	 }
+    }else{
+	strcpy(buf,"-2"); // Failure
+    } 
+*/    /* Sends Message */
+/*    if ((sendto(new_s,buf,sizeof(buf),0,(struct sockaddr *)&new_s,sizepf(struct sockaddr)))<0){
+	fprintf(stderr,"[MDIR sendto #1] : %s",strerror(errno));
+	continue;
+    }
+*/
+
+}
+
+
+void cdir(char *buffer, int buf_len){
+/*    char buf[MAXSIZE];
+    if ((size!=recv(new_s,buf,sizeof(buf),0))<0){
+	fprintf(stderr,"[recv] : %s",strerror(errno));
+	return -1;
+    }
+    char *root_path = "../root_dir"
+    char *full_path = "";
+    sprintf(full_path,"%s%s",root_path,name);
+    DIR *d = opendir(full_path);
+    if (d != NULL){ // Success
+	 char *command;
+	 char *temp_com = "cd ";
+         sprintf(command,"%s%s",temp_com,full_path);
+	 int rtr_val = system(command);
+	 if (rtr_val == 0){ // Successful Sys Call
+	    buf = "1";	
+	 }else{             // Failed Sys Call
+	    buf = "-1";
+	 }
+    }else{
+	buf = "-2"; // Failure
+    } 
+*/    /* Sends Message */
+/*    if ((sendto(new_s,buf,sizeof(buf),0,(struct sockaddr *)&new_s,sizepf(struct sockaddr)))<0){
+	fprintf(stderr,"[MDIR sendto #1] : %s",strerror(errno));
+	continue;
+    }
+*/
+}
 void quit() {
 	printf("Quit\n");
         exit(1);
