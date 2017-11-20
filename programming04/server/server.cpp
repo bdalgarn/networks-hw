229bf89bfdc64f68e7b0111e3b92b602ea0fec64
bdalgarn <bdalgarn@nd.edu>
2017-11-20 04:34:38 -0500

    Got rid of compiler errors

--- programming04/server/server.cpp
+++ programming04/server/server.cpp
@@ -6,6 +6,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <map>
+#include <queue>
 #define MAX_SIZE 10
 #define DEBUG 1
 using namespace std;
@@ -13,8 +15,19 @@ using namespace std;
 
 
 void *connection_handler(void *);
+void read_it(queue <char *> *, int);	
+void write_it(queue <char *> **, char *, map<char *,queue<char *> *> *, int);
+char * get_message(int sock, int mode);
+void check_error(ssize_t size);
 
-map<char *,queue *> queues;
+std::map<char *,std::queue<char *> *> queues;
+struct arg_t {
+		//queue <char *> *user_qs[10];
+		void * user_qs;
+		char * user;	
+		map <char *, queue <char *> *> *user_map; 		   
+		int sock;
+};
 
 //queue <queue<char *>> qs[10];
 //bzero(qs,sizeof(queue)*sizeof(char *));
@@ -23,15 +36,16 @@ map<char *,queue *> queues;
 int main(int argc , char *argv[]){
 
     /* Initializations */
-    queue <char *> qs[10];
-    bzero(qs,sizeof(queue)*sizeof(char *));
+    std::queue <char *> qs[10];
+
+    bzero(qs,sizeof(queue<char *>)*sizeof(char *)); 
     int sock , client_sock , c , *new_sock;
     struct sockaddr_in server , client;
     bzero((char *)&server,sizeof(server));
     bzero((char *)&client,sizeof(client));
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons( 8888 );
+    server.sin_port = htons(atoi(argv[1]));
     /* Building Socket */ 
     sock = socket(AF_INET , SOCK_STREAM , 0);
     if (sock == -1){
@@ -48,18 +62,20 @@ int main(int argc , char *argv[]){
     /* Accept on Socket */
     while((client_sock=accept(sock,(struct sockaddr *)&client,(socklen_t*)&c))){
         pthread_t client_thread;
-        new_sock = malloc(1);    // Allocate space
+        new_sock = (int *)malloc(1);    // Allocate space
         *new_sock = client_sock; // Duplicae address
 
-       char * user_name = "ayy lmao";
-       struct args {
-		queue <char *> user_qs[10] = &qs;
-		char * user = user_name;	
-		map <char *, queue <char *> *> user_map = &users; 		   
-		int sock = new_sock;
+       char * user_name = "Ben";
+
+       map <char *, queue <char *> *> users; 		   
+       arg_t args {
+		(void *)&qs, // user_qs
+		user_name, // user	
+		&users, // user_map	   
+		*new_sock // sock
        };
         /* Creates New Thread */
-        if(pthread_create(&client_thread,NULL,connection_handler,(void*)args)<0){
+        if(pthread_create(&client_thread,NULL,connection_handler,(void*)&args)<0){
             perror("could not create thread");
             return 1;
         }
@@ -74,12 +90,10 @@ int main(int argc , char *argv[]){
 } 
 
 
-
-
-
-void *connection_handler(void *args){
+void *connection_handler(void *_args){
+    arg_t *args = (arg_t *)_args;
     /* Initializations */
-    int sock = *(int*)args->sock;
+    int sock = args->sock;
     char * to_be_written,user;  // This is going to be what we need to write to the users queue
     char * to_be_recieved; // This is what we need to send to our client (messages from others in their queue)
     ssize_t read_size;
@@ -88,25 +102,31 @@ void *connection_handler(void *args){
 		       // Kinda lost about ideas on that tho so pls add whatever makes sense to you
 
     /* Casts vals back from void */
-    queue <char *> copy_q[10] = (queue <char *>)args->user_qs;
-    char * user_name = (char *)args->user;
-    map<char *,queue *> copy_map = (map<char *,queue *> *)args->user_map;
-    queue <char *> user_q = &(queue <char *>)copy_map[user_name];
+    queue <char *> *copy_q[10]; 
+    for (int i = 0; i < 10; i++) {
+	copy_q[i] = &((queue <char *> *)args->user_qs)[i];
+    }
+    ;
+    char * user_name = args->user;
+    map<char *,queue<char *> *> copy_map = *(map<char *,queue<char *> *> *)args->user_map;
+    queue <char *> *user_q = copy_map[user_name];
 
     if(mode){
 	    for (int i = 0; i < MAX_SIZE; i++){
-		 if (&copy_q[i]==user_q){
-		   read_it(*user_q,(int*)args->sock);
+		 if (copy_q[i]==user_q){
+		   read_it(user_q,args->sock);
 		}
 	    }
     }else{    
-	   write_it(copy_q,user_name,&opy_map,(int*)args->sock);
+	   write_it(&copy_q[0],user_name,&copy_map,args->sock);
     }
+    void * return_ptr = NULL;
+    return return_ptr;
 }
 
 
 
-char * get_mesage(int *sock, int mode){
+char * get_message(int sock, int mode){
     /* Build Probe */
     char * buf;
     if (mode==1){
@@ -118,11 +138,12 @@ char * get_mesage(int *sock, int mode){
     }
     /* Send Probe */
     ssize_t size = send(sock,buf,sizeof(buf),0);
-    bzero(cat,sizeof(cat));
-    if (DEBUG==1) checkerror(size);
+    char *cat = (char *)malloc(BUFSIZ*4);
+    bzero(cat,sizeof(*cat));
+    if (DEBUG==1) check_error(size);
     /* Get Message */
-    char *client_message[BUFSIZ];
-    char *cat[BUFSIZ*4];
+    char client_message[BUFSIZ];
+    ssize_t read_size;
     while((read_size = recv(sock,client_message,BUFSIZ,0))>0){
 	strcat(cat,client_message);
     }
@@ -130,28 +151,32 @@ char * get_mesage(int *sock, int mode){
     return cat;
 }
 
-void read_it(queue <char *> &messages, int sock){	
-    char * to_send = messages.pop();
+void read_it(queue <char *> * _messages, int sock){	
+    queue<char *> messages = *_messages;
+    char * to_send = messages.back();
+    messages.pop();
     /* Send Probe */
     ssize_t size = send(sock,to_send,sizeof(to_send),0);
-    bzero(cat,sizeof(cat));
-    if (DEBUG==1) checkerror(size); 
+    //bzero(cat,sizeof(cat));
+    if (DEBUG==1) check_error(size); 
 }
 
-char * format_msg(char * user,int mode,char *data,map<char *,queue *> dict){
-     char *formatted_str[BUFSIZ*4];
+char * format_msg(char * user,int mode,char *data,map<char *,queue<char *> *> dict){
+     char *formatted_str;
+     formatted_str = (char *)malloc(BUFSIZ*4);
+     strcpy(formatted_str, ""); 
      if (mode==0){ // Message Handler
-           strcat(formatted_str,'C');
-	   strcat(formatted_str,',');
+           strcat(formatted_str,"C");
+	   strcat(formatted_str,",");
            strcat(formatted_str,user);
-           strcat(formatted_str,',');
+           strcat(formatted_str,",");
            strcat(formatted_str,data); 
      }else if (mode==1){ // List handler
-	   strcat(formatted_str,'C');
-           map<char *,queue *>::iterator it;
+	   strcat(formatted_str,"C");
+           map<char *,queue<char *> *>::iterator it;
            for (it=dict.begin(); it !=dict.end();it++){
 		if (it->second!=NULL){
-           		strcat(formatted_str,',');
+           		strcat(formatted_str,",");
            		strcat(formatted_str,it->first);
 	        }
 	   } 
@@ -160,52 +185,52 @@ char * format_msg(char * user,int mode,char *data,map<char *,queue *> dict){
 }
 
 
-void write_it(queue <char *>  qs, char * user, map<char *,queue *> dict, int *sfd)
+void write_it(queue <char *> ** qs, char * user, map<char *,queue<char *> *> * dict, int sfd) {
     /* Inits */
-    int sock = *sfd;
+    int sock = sfd;
     ssize_t read_size;
-    char *message, client_message[BUFSIZ], recipient[BUFSIZ];
-    char *cat_buf[BUFSIZ*4],formatted_buf[BUFSIZ*4];
+    char *message, *client_message, *recipient;
+    char *formatted_buf;
+    char *cat_buf;
 
     /* Read Message Type */
-    read_size = recv(sock ,client_message,BUFSIZ,0));
-    if (DUBUG==1) checkerror(read_size);
-    bzero(client_message,sizeof(client_message));
+    read_size = recv(sock ,client_message,BUFSIZ,0);
+    if (DEBUG==1) check_error(read_size);
+    bzero(client_message,BUFSIZ*4);
 
     switch(client_message[0]){
        case 'B': /* Broadcast Message */
            cat_buf = get_message(sock,1); 
            for (int i = 0; i < MAX_SIZE; i++){
-              if (&qs[i]!=&dict[user]){
-                  formatted_buf = format_msg(user,0,cat_buf,dict);
-		  qs[i].push(formatted_buf);
+              if (qs[i] != (*dict)[user]){
+                  formatted_buf = format_msg(user,0,cat_buf,*dict);
+		  qs[i]->push(formatted_buf);
 	      }
 	   } 
 	   break;
        case 'L': /* Personal Message */
-	   formatted_buf = format_msg(user,1,NULL,dict);
+	   formatted_buf = format_msg(user,1,NULL,*dict);
     	   read_size = send(sock,formatted_buf,sizeof(formatted_buf),0);
        case 'P': /* Personal Message */
            recipient = get_message(sock,0);
 	   cat_buf = get_message(sock,1);
            for (int i = 0; i < MAX_SIZE; i++){
-              if (&qs[i]==&dict[recipient]){
-                  formatted_buf = format_msg(user,0,cat_buf,dict){
-		  qs[i].push(formatted_buf);
+              if (qs[i]==(*dict)[recipient]){
+                  formatted_buf = format_msg(user,0,cat_buf,*dict);
+		  qs[i]->push(formatted_buf);
 	      }
 	   }
            break;
 
        case 'E': /* Cleanup Message */
            fprintf(stdout,"Peace fam\n");	   
-           dict[user] = NULL;
+           (*dict)[user] = NULL;
            break;
 
        default:
  	   break;
     }
-    free(sock); 
-    return 0;
+    close(sock); 
 }
 
 void check_error(ssize_t size){
